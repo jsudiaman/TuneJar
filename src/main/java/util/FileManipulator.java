d0d9760a1f10aa033a5adbda23b1e8a878aae455
Jonathan Sudiaman <sudiamanj@wit.edu>
2015-10-30 00:14:15 -0400

    Add GitHub issue number

--- src/main/java/util/FileManipulator.java
+++ src/main/java/util/FileManipulator.java
@@ -122,7 +122,7 @@ public final class FileManipulator {
 	 *            A File object that is a directory.
 	 * @return A collection containing all the Song objects.
 	 */
-	// TODO Multithread this method to optimize runtime
+	// TODO Fix Issue #4
 	public static Collection<Song> getSongs(File directory) {
 		// Initialize the set or return an empty set if necessary.
 		Set<Song> set = new HashSet<>();
