860512cdf75f7744cac2984e47bd59e4b0d307d2
Jonathan Sudiaman <sudiamanj@wit.edu>
2015-12-31 02:28:08 -0500

    Update build_script.sh

--- build/build_script.sh
+++ build/build_script.sh
@@ -1,3 +1,3 @@
 cp target/TuneJar-v0.1-SNAPSHOT-jar-with-dependencies.jar TuneJar.jar
 cp build/TuneJar.bat TuneJar.bat
-zip TuneJar.zip TuneJar.jar TuneJar.bat
+zip TuneJar.zip TuneJar.jar LICENSE.txt TuneJar.bat
