e61ae9531a539f4cedf4ae27061b8fa6af24855b
Jonathan Sudiaman <sudiamanj@wit.edu>
2016-01-03 15:42:47 -0500

    Change TuneJar home directory

--- src/main/java/tunejar/config/Defaults.java
+++ src/main/java/tunejar/config/Defaults.java
@@ -1,6 +1,7 @@
 package tunejar.config;
 
 import java.io.File;
+import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
@@ -33,29 +34,35 @@ public class Defaults {
 	public static final Map<String, String> THEME_MAP;
 	public static final String[] SORT_ORDER;
 	public static final Set<File> DIRECTORIES;
+	public static final String TUNEJAR_HOME;
 
 	static {
-		GET_SONGS_TIMEOUT = 5 * 60;
-		LOG_FILE_LIMIT = 15;
-		TEST_MP3 = "/BitQuest.mp3";
-		TEST_MP4 = "/BitQuest.m4a";
-		LOG_FOLDER = "logs";
-		MAX_LOOPS = 1000;
-		PLAYER_FXML = "/fxml/Player.fxml";
-		THEME = "Modena";
-		ICON = "/img/icon.png";
-		OPTIONS_FILE = "options.json";
-		VOLUME = 1.0;
-		SORT_ORDER = new String[0];
-		DIRECTORIES = new HashSet<>();
-
-		// Theme Map
+		// Theme map
 		String modena = Player.class.getResource("/theme/Modena.css").toString();
 		String darkTheme = Player.class.getResource("/theme/Dark Theme.css").toString();
 		Map<String, String> themeMap = new LinkedHashMap<>();
 		themeMap.put("Modena", modena);
 		themeMap.put("Dark Theme", darkTheme);
 		THEME_MAP = Collections.unmodifiableMap(themeMap);
+
+		// TuneJar home
+		TUNEJAR_HOME = Paths.get(System.getProperty("user.home"), "Documents", "TuneJar").toString();
+		new File(TUNEJAR_HOME).mkdirs();
+		OPTIONS_FILE = Paths.get(TUNEJAR_HOME, "options.json").toString();
+
+		// Misc. constants
+		LOG_FOLDER = "logs";
+		GET_SONGS_TIMEOUT = 5 * 60;
+		LOG_FILE_LIMIT = 5;
+		TEST_MP3 = "/BitQuest.mp3";
+		TEST_MP4 = "/BitQuest.m4a";
+		MAX_LOOPS = 1000;
+		PLAYER_FXML = "/fxml/Player.fxml";
+		THEME = "Modena";
+		ICON = "/img/icon.png";
+		VOLUME = 1.0;
+		SORT_ORDER = new String[0];
+		DIRECTORIES = new HashSet<>();
 	}
 
 }
