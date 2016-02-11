5882b3279d88347061c43d6cda2c4b8881393518
Jonathan Sudiaman <jsudiaman@gmail.com>
2016-02-11 12:19:41 -0500

    Update copyright information

--- src/test/java/com/sudicode/tunejar/menu/FileMenuTest.java
+++ src/test/java/com/sudicode/tunejar/menu/FileMenuTest.java
@@ -1,22 +1,20 @@
-// @formatter:off
 /*
  * TuneJar <http://sudicode.com/tunejar/>
  * Copyright (C) 2016 Jonathan Sudiaman
  *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later
- * version.
- * 
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- * 
- * You should have received a copy of the GNU General Public License along with
- * this program. If not, see <http://www.gnu.org/licenses/>.
- */
-// @formatter:on
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 package com.sudicode.tunejar.menu;
 
@@ -34,7 +32,6 @@ import java.util.function.Supplier;
 
 import javafx.collections.ObservableList;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
 
 public class FileMenuTest extends IntegrationTest {
 
@@ -45,11 +42,10 @@ public class FileMenuTest extends IntegrationTest {
 
         getDriver().clickOn("File").clickOn("New").clickOn("Playlist...");
         TextField name = GuiTest.find(Defaults.PLAYLIST_NAME);
-        getDriver().type(KeyCode.A, KeyCode.B, KeyCode.C, KeyCode.DIGIT1, KeyCode.DIGIT2, KeyCode.DIGIT3);
-        assertTrue(name.getText().equals("abc123"));
+        name.setText("FileMenuTest_testNewPlaylist");
         getDriver().clickOn("OK");
-        assertTrue(items.get().get(index).getName().equals("abc123"));
-        assertTrue(Files.exists(Defaults.PLAYLISTS_FOLDER.resolve("abc123.m3u")));
+        assertTrue(items.get().get(index).getName().equals("FileMenuTest_testNewPlaylist"));
+        assertTrue(Files.exists(Defaults.PLAYLISTS_FOLDER.resolve("FileMenuTest_testNewPlaylist.m3u")));
     }
 
 }
