316fe4ff6c9e0c0bb9561ce036948a26b0f18905
Jonathan Sudiaman <jsudiaman@gmail.com>
2017-01-02 11:37:46 -0500

    Update travis config

--- src/test/java/com/sudicode/tunejar/menu/ThemeMenuTest.java
+++ src/test/java/com/sudicode/tunejar/menu/ThemeMenuTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
-
 public class ThemeMenuTest {
 
     private Gui gui = Gui.getInstance();
