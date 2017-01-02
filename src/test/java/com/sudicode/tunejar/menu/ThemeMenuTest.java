e33d4aca558e28bbd8c30c27cf5e12dcb0657579
Jonathan Sudiaman <jsudiaman@gmail.com>
2017-01-02 11:59:05 -0500

    Update ThemeMenuTest

--- src/test/java/com/sudicode/tunejar/menu/ThemeMenuTest.java
+++ src/test/java/com/sudicode/tunejar/menu/ThemeMenuTest.java
@@ -2,25 +2,35 @@ package com.sudicode.tunejar.menu;
 
 import com.sudicode.tunejar.player.Gui;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import static com.jayway.awaitility.Awaitility.await;
+import static com.jayway.awaitility.Duration.FIVE_SECONDS;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 public class ThemeMenuTest {
 
+    private static final Logger logger = LoggerFactory.getLogger(ThemeMenuTest.class);
+
     private Gui gui = Gui.getInstance();
 
     @Test
     public void testDarkTheme() throws Exception {
-        gui.getRobot().clickOn("#themeSelector").sleep(250).clickOn("Dark Theme");
-        assertThat(gui.getPlayer().getScene().getStylesheets().get(0), endsWith("Dark%20Theme.css"));
+        gui.getRobot().clickOn("#themeSelector");
+        await().atMost(FIVE_SECONDS).until(() -> gui.getRobot().clickOn("Dark Theme"));
+        logger.info("Clicked on Dark Theme");
+        await().atMost(FIVE_SECONDS).until(() -> gui.getPlayer().getScene().getStylesheets().get(0), endsWith("Dark%20Theme.css"));
         assertThat(gui.getPlayer().getOptions().getTheme(), is(equalTo(("Dark Theme"))));
     }
 
     @Test
     public void testModena() throws Exception {
-        gui.getRobot().clickOn("#themeSelector").sleep(250).clickOn("Modena");
-        assertThat(gui.getPlayer().getScene().getStylesheets().get(0), endsWith("Modena.css"));
+        gui.getRobot().clickOn("#themeSelector");
+        await().atMost(FIVE_SECONDS).until(() -> gui.getRobot().clickOn("Modena"));
+        logger.info("Clicked on Modena");
+        await().atMost(FIVE_SECONDS).until(() -> gui.getPlayer().getScene().getStylesheets().get(0), endsWith("Modena.css"));
         assertThat(gui.getPlayer().getOptions().getTheme(), is(equalTo("Modena")));
     }
 
